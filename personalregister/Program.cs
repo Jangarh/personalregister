ecb15f05ce82465b50850d0ffad0d2517cb54a26
Elev <Elev@ELEVDATOR-27>
2021-12-01 10:08:54 +0100

    Add project files.

---
+++ personalregister/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace personalregister
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tell us your name and your salary");
+            Console.Write("Type your name: ");
+
+            string myName;
+            myName = Console.ReadLine();
+
+            Console.Write("Type your salary: ");
+            int mySalary = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Your name is: " + myName);
+            Console.WriteLine("Your salary is: " + mySalary);
+
+            Console.ReadLine();
+
+        }
+    }
+}
